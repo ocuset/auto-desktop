5073e7a4b308d8a2be71d93a8353acff4d7651fa
ocuset <84349479+ocuset@users.noreply.github.com>
2022-05-22 14:34:51 +0200

    Add files via upload

---
+++ autodesktop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Welcome to AutoDebian, a script made by pico to automate the installation of debian with IceWM"
+echo "feel free to modify it, also, please run this as root"
+apt-get install icewm pulseaudio lxappearance git curl sudo nano vim lightdm lightdm-gtk-greeter
+&& systemctl enable lightdm
+&& sudo reboot
+
