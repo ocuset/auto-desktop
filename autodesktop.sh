c01b603b09b7864af7d852ed6e6c036839541150
ocuset <84349479+ocuset@users.noreply.github.com>
2022-05-22 14:39:46 +0200

    Update autodesktop.sh

--- autodesktop.sh
+++ autodesktop.sh
@@ -2,7 +2,7 @@
 echo "Welcome to AutoDebian, a script made by pico to automate the installation of debian with IceWM"
 echo "feel free to modify it, also, please run this as root"
 apt-get install icewm pulseaudio lxappearance git curl sudo nano vim lightdm lightdm-gtk-greeter
-&& apt-get install alacritty xorg
-&& systemctl enable lightdm
-&& sudo reboot
+apt-get install alacritty xorg
+systemctl enable lightdm
+sudo reboot
 
